057b155f34199346abee70a3eccb352e422b4d0a
B209678-2021 <B209678-2021@users.noreply.github.com>
2021-11-26 17:45:28 +0000

    second draft

--- step1.py
+++ step1.py
@@ -1,48 +1,60 @@
 #Shebang line
 #!/usr/local/bin/python3
 
-#Import Modules 
+#Import Modules
 import os, sys, shutil, subprocess
+print("\nImported os, sys, shutil and subprocess\n")
 
-#User entry functions 
-def user(Protein, Taxonomic_group):
-	import string 
-	print("The following details have been provided:\n\tProtein family: ",Protein, "\n\tTaxonomic_group: ",Taxonomic_group)
-	for character in protein:
-		if character not in string.ascii_letters: 
-			return print("\nInvalid entry: re-enter protein family name please")
-		if character not in string.ascii_letters:	
-			return print("\nInvalid entry")
+os.system('clear')
+
+#______________________________________________#
+
+#User entry functions
+def user_entry(protein, taxonomic_group):
+        import string
+        print("The following details have been provided:\n\tProtein family: ",protein, "\n\tTaxonomic_group: ",taxonomic_group)
+
 
 #Store output in an ordered Dictionary
 
 questions ={}
-questions["protein_family"] = input("Please enter protein family:\n")
+questions["protein"] = input("Please enter protein family:\n")
 questions["taxonomic_group"] = input("Please enter taxonomic group:\n")
 
-#Combine protein family and taxonomic group
-user(*list(questions.values()))
-print("\n\nComplete\n\n")
+protein = questions["protein"]
+taxonomic_group = questions["taxonomic_group"]
+user_entry(*list(questions.values()))
 
-#retrieve the sequences
-command = "esearch -db protein -query "{1}[Organism] AND {0}[Protein name] NOT PARTIAL' | efetch -db protein -format fasta ".
+#os.environ["protein"] = protein
+#os.environ["taxonomic_group"] = taxonomic_group
+#Retrieve the sequences using Esearch and Efetch from EDirect
+command = "esearch -db protein -query '{0}[Organism] AND {1}[Protein] NOT PARTIAL' | efetch -db protein -format fasta > {0}.search.fasta ".format(questions["taxonomic"],questions["protein"])
+subprocess.call(command, shell = True)
 
-#Run in the command bash
-subprocess(command, shell = True)
+
 
 #Limit the number of sequences
 
 
-#Align multiple sequences in clustalo 
-#clustalo_command = r'clustalo -i protein.fa -o protein.msf -t4'
+
+
+
+#Align multiple sequences in clustalo
+#Convert from fasta to msf format for the plotcon step
+#clustalo_command = r'clustalo -i help.fa -o glu.align.msf -t4  protein --outfmt msf -v'
 #subprocess.call(clustalo_command, shell=True)
 
-clustalo -i help.fa -o glu.align.msf -t4  protein --outfmt msf -v
+#clustalo -i help.fa -o glu.align.msf -t4  protein --outfmt msf -v
 
 
-#Make a plotcon
+#Make a plotcon 
+#Ps saves graph
+#x11 prints to the screen
 #plotcon -sformat msf glu.align.msf -winsize 10 -graph ps
 #plotcon -sformat msf glu.align.msf -winsize 10 -graph x11
+
+
+
 
 
 
