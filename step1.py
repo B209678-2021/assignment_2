87ad23dcbd67cb3649410163a751fedaeea9b9b9
B209678-2021 <B209678-2021@users.noreply.github.com>
2021-11-27 10:07:17 +0000

    third draft

--- step1.py
+++ step1.py
@@ -10,9 +10,9 @@ os.system('clear')
 #______________________________________________#
 
 #User entry functions
-def user_entry(protein, taxonomic_group):
+def user(Protein, Taxonomic_group):
         import string
-        print("The following details have been provided:\n\tProtein family: ",protein, "\n\tTaxonomic_group: ",taxonomic_group)
+        print("The following details have been provided:\n\tProtein family: ",Protein, "\n\tTaxonomic_group: ",Taxonomic_group)
 
 
 #Store output in an ordered Dictionary
@@ -23,13 +23,12 @@ questions["taxonomic_group"] = input("Please enter taxonomic group:\n")
 
 protein = questions["protein"]
 taxonomic_group = questions["taxonomic_group"]
-user_entry(*list(questions.values()))
+user(*list(questions.values()))
 
-#os.environ["protein"] = protein
-#os.environ["taxonomic_group"] = taxonomic_group
+
 #Retrieve the sequences using Esearch and Efetch from EDirect
-command = "esearch -db protein -query '{0}[Organism] AND {1}[Protein] NOT PARTIAL' | efetch -db protein -format fasta > {0}.search.fasta ".format(questions["taxonomic"],questions["protein"])
-subprocess.call(command, shell = True)
+search_command = "esearch -db protein -query '{0}[Organism] AND {1}[Protein] NOT PARTIAL' | efetch -format fasta > {0}.{1}.fasta ".format(questions["taxonomic"],questions["protein"])
+subprocess.call(search_command, shell = True)
 
 
 
